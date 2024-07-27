241ea4db57e93fa95c043a2892beb71b9d911136
mykeshale <mykeshale@gmail.com>
2024-07-27 17:14:52 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Node App Running');
+})
+
+app.listen(port, () => {
+  console.log(`Node App listening at http://localhost:${port}`)
+})
+
